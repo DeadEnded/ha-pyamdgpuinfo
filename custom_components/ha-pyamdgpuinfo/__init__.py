99ab27228efc5278b7ba644daae6b7851b9f4516
DeadEnd <45110141+DeadEnded@users.noreply.github.com>
2021-08-16 16:12:30 -0400

    Update __init__.py
    Adding code for config-flow

--- custom_components/ha-pyamdgpuinfo/__init__.py
+++ custom_components/ha-pyamdgpuinfo/__init__.py
@@ -1,1 +1,24 @@
 """The ha-pyamdgpuinfo component."""
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import CONF_RESOURCES, DOMAIN, PLATFORMS
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up AMD GPU from a config entry."""
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+
+def find_resources_in_config_entry(config_entry):
+    """Find the configured resources in the config entry."""
+    if CONF_RESOURCES in config_entry.options:
+        return config_entry.options[CONF_RESOURCES]
+    return config_entry.data[CONF_RESOURCES]
