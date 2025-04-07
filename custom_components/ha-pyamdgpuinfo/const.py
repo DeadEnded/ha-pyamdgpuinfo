0fd57b3fa4792bf2d5be3bacc9f202b59146b443
DeadEnd <45110141+DeadEnded@users.noreply.github.com>
2025-04-07 11:09:54 -0400

    Update const.py
    Updated to align with changes in Home Assistant core.

--- custom_components/ha-pyamdgpuinfo/const.py
+++ custom_components/ha-pyamdgpuinfo/const.py
@@ -1,17 +1,15 @@
 """ The AMD GPU component."""
 
 from homeassistant.components.sensor import (
-    DEVICE_CLASS_POWER,
-    DEVICE_CLASS_TEMPERATURE,
-    DEVICE_CLASS_VOLTAGE,
+    SensorDeviceClass,
 )
 
 from homeassistant.const import (
     CONF_RESOURCES,
-    DATA_MEBIBYTES,
+    UnitOfInformation,
     PERCENTAGE,
-    TEMP_CELSIUS,
-    POWER_WATT,
+    UnitOfTemperature,
+    UnitOfPower,
     ATTR_VOLTAGE,
 )
 
@@ -25,12 +23,12 @@ PLATFORMS = ["sensor"]
 
 # Schema: [name, unit of measurement, icon, device class, flag if mandatory arg]
 SENSOR_TYPES = {
-    "gpu_temperature": ["GPU temperature", TEMP_CELSIUS, "mdi:thermometer", DEVICE_CLASS_TEMPERATURE, False, ],
-    "vram_size": ["VRAM Size", DATA_MEBIBYTES, "mdi:memory", None, False],
-    "vram_usage": ["VRAM Usage", DATA_MEBIBYTES, "mdi:memory", None, False],
+    "gpu_temperature": ["GPU temperature", UnitOfTemperature.CELSIUS, "mdi:thermometer", SensorDeviceClass.TEMPERATURE, False, ],
+    "vram_size": ["VRAM Size", UnitOfInformation.MEBIBYTES, "mdi:memory", None, False],
+    "vram_usage": ["VRAM Usage", UnitOfInformation.MEBIBYTES, "mdi:memory", None, False],
     "vram_percent": ["VRAM Usage (percent)", PERCENTAGE, "mdi:memory", None, False],
-    "gtt_size": ["GTT Size", DATA_MEBIBYTES, "mdi:memory", None, False],
-    "gtt_usage": ["GTT Usage", DATA_MEBIBYTES, "mdi:memory", None, False],
+    "gtt_size": ["GTT Size", UnitOfInformation.MEBIBYTES, "mdi:memory", None, False],
+    "gtt_usage": ["GTT Usage", UnitOfInformation.MEBIBYTES, "mdi:memory", None, False],
     "gtt_percent": ["GTT Usage (percent)", PERCENTAGE, "mdi:memory", None, False],
     "sclk_max": ["Shader Clock Max", "GHz", "mdi:chip", None, False],
     "sclk_usage": ["Shader Clock Usage", "GHz", "mdi:chip", None, False],
@@ -39,9 +37,9 @@ SENSOR_TYPES = {
     "mclk_usage": ["Memory Clock Usage", "GHz", "mdi:chip", None, False],
     "mclk_percent": ["Memory Clock Usage (percent)", PERCENTAGE, "mdi:chip", None, False],
     "query_load": ["Overall GPU load", PERCENTAGE, "mdi:chip", None, False],
-    "query_power": ["Power consumption", POWER_WATT, "mdi:lightning-bolt", DEVICE_CLASS_POWER, False],
-    "query_northbridge_voltage": ["Northbrige Voltage", ATTR_VOLTAGE, "mdi:lightning-bolt", DEVICE_CLASS_VOLTAGE, False],
-    "query_graphics_voltage": ["Graphics Voltage", ATTR_VOLTAGE, "mdi:lightning-bolt", DEVICE_CLASS_VOLTAGE, False],
+    "query_power": ["Power consumption", UnitOfPower.WATT, "mdi:lightning-bolt", SensorDeviceClass.POWER, False],
+    "query_northbridge_voltage": ["Northbrige Voltage", ATTR_VOLTAGE, "mdi:lightning-bolt", SensorDeviceClass.VOLTAGE, False],
+    "query_graphics_voltage": ["Graphics Voltage", ATTR_VOLTAGE, "mdi:lightning-bolt", SensorDeviceClass.VOLTAGE, False],
     "texture_addresser": ["Texture Addresser (percent)", PERCENTAGE, "mdi:chip", None, False],
     "shader_export": ["Shader Export (percent)", PERCENTAGE, "mdi:chip", None, False],
     "shader_interpolator": ["Shader Interpolator (percent)", PERCENTAGE, "mdi:chip", None, False],
